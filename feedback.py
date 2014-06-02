ab970849dcdca8a8165362296e2a2d76128e8d52
Roman Levin <roman.levin@stxnext.pl>
2014-06-02 16:52:39 +0200

    Sending emails.

--- feedback.py
+++ feedback.py
@@ -1,13 +1,13 @@
 import json
 import csv
 from collections import defaultdict
+import smtplib
+import argparse
+
+USERNAME, PASSWORD = '', ''
 
 USER_FILE = 'users.json'
-FEEDBACK_FILE = 'personal_feedback.csv'
-
-FROM_NAME = "Example Name"
-FROM_EMAIL = "name@example.com"
-
+FEEDBACK_FILE = 'safe_feedback.csv'
 
 HEADER_TEMPLATE = u"""From: {from_name} <{from_email}>
 To: {to_name} <{to_email}>
@@ -66,19 +66,43 @@ def parse_feedback():
     return feedback_dict
 
 
-def format_feedback(name, users, feedback):
-    email = users[name]
+def format_feedback(name, email, feedback, username):
     header = HEADER_TEMPLATE.format(
-        to_name=name, to_email=email, from_name=FROM_NAME,
-        from_email=FROM_EMAIL)
+        to_name=name, to_email=email, from_name=username,
+        from_email=username)
     message = header
     for feedback_item in feedback[name]:
         message += ROW_TEMPLATE.format(**feedback_item)
     return message
 
 
+def connect(username, password):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    return server
+
+
+def send_email(name, email, feedback, server, username):
+    message = format_feedback(name, email, feedback, username)
+    server.sendmail(username, [email], message)
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    args = parser.parse_args()
+    return args.username, args.password
+
+
 if __name__ == '__main__':
+    username, password = parse()
+    server = connect(username, password)
     users = parse_users()
     feedback = parse_feedback()
     for name in feedback:
-        print format_feedback(name, users, feedback)
+        email = users[name]
+        send_email(name, email, feedback, server, username)
+    server.quit()
