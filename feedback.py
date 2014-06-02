8b9288422aec35b9320b5df78206c1be32814e2f
Roman Levin <roman.levin@stxnext.pl>
2014-06-02 16:14:17 +0200

    Email format.

--- feedback.py
+++ feedback.py
@@ -5,6 +5,29 @@ from collections import defaultdict
 USER_FILE = 'users.json'
 FEEDBACK_FILE = 'personal_feedback.csv'
 
+FROM_NAME = "Example Name"
+FROM_EMAIL = "name@example.com"
+
+
+HEADER_TEMPLATE = u"""From: {from_name} <{from_email}>
+To: {to_name} <{to_email}>
+Subject: Feedback
+
+This is your feedback:
+
+"""
+
+ROW_TEMPLATE = u"""
+*****
+Date: {date}
+
+Grade: {grade}
+
+Do: {do}
+
+Don't: {dont}
+"""
+
 
 def u(s):
     try:
@@ -29,7 +52,7 @@ def parse_users():
 
 def parse_feedback():
     feedback_dict = defaultdict(list)
-    fieldnames = ('timestamp', 'name', 'grade', 'do', '_', 'dont')
+    fieldnames = ('date', 'name', 'grade', 'do', '_', 'dont')
     exclude = ('name', '_')
     with open(FEEDBACK_FILE, 'rb') as feedback_file:
         feedback_reader = csv.DictReader(
@@ -43,6 +66,19 @@ def parse_feedback():
     return feedback_dict
 
 
+def format_feedback(name, users, feedback):
+    email = users[name]
+    header = HEADER_TEMPLATE.format(
+        to_name=name, to_email=email, from_name=FROM_NAME,
+        from_email=FROM_EMAIL)
+    message = header
+    for feedback_item in feedback[name]:
+        message += ROW_TEMPLATE.format(**feedback_item)
+    return message
+
+
 if __name__ == '__main__':
     users = parse_users()
     feedback = parse_feedback()
+    for name in feedback:
+        print format_feedback(name, users, feedback)
